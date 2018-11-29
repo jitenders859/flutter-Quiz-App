00c7ce85169bc9381f2df529cf3ea15599da124a
Jitender Singh <jitenders859@gmail.com>
2018-11-29 13:56:46 +0530

    Quiz App With Rest Api integration

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+import 'package:quiz_app/quiz.dart';
+import 'package:quiz_app/quizsecond.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: QuizSecondApp(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.white),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  Quiz quiz;
+  List<Results> results;
+
+  Future<void> fetchQuestions() async {
+    var res = await http.get('https://opentdb.com/api.php?amount=20');
+    var decRes = jsonDecode(res.body);
+    
+    print(decRes);
+    quiz = Quiz.fromJson(decRes);
+    results = quiz.results;
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quiz App"),
+        elevation: 0.0,
+        centerTitle: true,
+      ),      
+      body: FutureBuilder(
+        future: fetchQuestions(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch(snapshot.connectionState) {
+            case ConnectionState.none:
+            return Text("Press Button to Start.");
+            
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+            case ConnectionState.done:
+            if(snapshot.hasError) return Container();
+            return questionList();
+
+          }
+          return null;
+        } ,
+      ),
+    );
+  }
+
+  ListView questionList() {
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.white,
+          elevation: 0.0,
+          child: ExpansionTile(
+            title: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  results[index].question,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                )
+              ],
+            )
+          ),
+        );
+      },
+    );
+  }
+
+}
+
